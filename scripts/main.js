2f84b971e2951691c4d992e454e74cc7dd6c9c1a
Joel Jackson <jackson.joel@gmail.com>
2013-11-04 20:01:51 -0800

    Modifies selected to only allow two players selected at once.

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,27 @@ angular.module("PingPong").controller("PlayersController", function($scope, angu
   $scope.data = {
     players: []
   }
+
   angularFire(ref, $scope, 'data');
 
   $scope.addUser = function() {
     $scope.data.players.push({name: $scope.newUserName});
   };
+});
+
+angular.module('PingPong').directive('inputDisabled', function($parse){
+  return {
+    restrict: "A", 
+    link: function(scope, element, attributes, controller){
+      scope.$watch("scope.player", function(newValue, oldValue){
+        if(_.filter(scope.$parent.data.players, function(player){ return player.selected;}).length >= 2 &&
+           !scope.player.selected){
+          element.attr('disabled', 'true');
+        } else {
+          element.removeAttr('disabled');
+        }
+        
+      }, true);
+    }
+  }
 });
