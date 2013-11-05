88a93378f0d110447b83ebccb1eaa4c480bfce21
Joel Jackson <jackson.joel@gmail.com>
2013-11-04 19:05:05 -0800

    Scopes all of firebase in a data object.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 angular.module("PingPong", ["firebase"]);
 angular.module("PingPong").controller("PlayersController", function($scope, angularFire){
-  var url = 'https://honest-ladder.firebaseio.com/players';
+  var url = 'https://honest-ladder.firebaseio.com/';
   var ref = new Firebase(url);
-  $scope.players = [];
-  angularFire(ref, $scope, 'players');
+  $scope.data = {
+    players: []
+  }
+  angularFire(ref, $scope, 'data');
 
   $scope.addUser = function() {
-    $scope.players.push({name: $scope.newUserName});
+    $scope.data.players.push({name: $scope.newUserName});
   };
 });
