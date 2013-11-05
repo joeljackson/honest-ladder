3c3ace52a5e27b736eed74068e8e51209c81478a
Joel Jackson <jackson.joel@gmail.com>
2013-11-04 18:59:13 -0800

    Spacing.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 angular.module("PingPong", ["firebase"]);
 angular.module("PingPong").controller("PlayersController", function($scope, angularFire){
-	var url = 'https://honest-ladder.firebaseio.com/players';
-	var ref = new Firebase(url);
-	$scope.players = [];
-	angularFire(ref, $scope, 'players');
+  var url = 'https://honest-ladder.firebaseio.com/players';
+  var ref = new Firebase(url);
+  $scope.players = [];
+  angularFire(ref, $scope, 'players');
 
-  	$scope.addUser = function() {
-    	$scope.players.push({name: $scope.newUserName});
-  	};
+  $scope.addUser = function() {
+    $scope.players.push({name: $scope.newUserName});
+  };
 });
