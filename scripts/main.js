24c7b512eb744f3aed19bb0e403b2a0d57caecdf
Justin Ricaurte <jricaurte@thehonestcompany.com>
2013-11-04 18:54:11 -0800

    put data in /players

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 angular.module("PingPong", ["firebase"]);
 angular.module("PingPong").controller("PlayersController", function($scope, angularFire){
-	var url = 'https://honest-ladder.firebaseio.com/';
+	var url = 'https://honest-ladder.firebaseio.com/players';
 	var ref = new Firebase(url);
 	$scope.players = [];
 	angularFire(ref, $scope, 'players');
